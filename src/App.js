cc9d34d48c391f5698c7620da7a73fd968eb750f
Anjeongkyun <dkswjdrbs12@naver.com>
2022-03-27 18:18:06 +0900

    기본 기능 Study 마지막 Commit,
    이후부턴 페이지 제작 예정

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import styles from './App.module.css'
 
     function App() {
         return <div className = "App">
-            <Hello />
-            <div className={styles.box}>Hello</div>
+            <Hello age = {10}/>
+            <Hello age = {20} />
+            <Hello age = {30} />
+            {/* <div className={styles.box}>Hello</div> */}
         </div>;
     }
 
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,7 +1,25 @@
 import World from "./World";
 import styles from "./Hello.module.css";
+import { useState } from "react";
+import UserName from "./UserName";
 
-export default function Hello() {
+export default function Hello({age}) {
+    console.log(age);
+    const msg = age > 19 ? "성인" : "미성년자";
+    // let name = "Mike";
+    
+    //[변수명, 변경해주는 함수]
+    const [name, setName] = useState('Mike');
+
+    function changeName() {
+        // name = name ==="Mike" ? "Jane" : "Mike";
+        // document.getElementById("name").innerText = name;
+
+        const newName = name ==="Mike" ? "Jane" : "Mike";
+        console.log(name);
+        setName(newName);
+
+    }
 
     function showName(){
         console.log("jkan");
@@ -17,12 +35,25 @@ export default function Hello() {
 
     return (              
     <div>
-    <p>Hello</p>
-    <World />
-    <div className={styles.box}>Hello</div>
+    {/* <p>Hello</p> */}
+    {/* <World /> */}
+    {/* <div className={styles.box}>Hello</div> */}
+    <br></br>
+
     <button onClick={showName}> show name</button>
+    <br></br>
+
     <button onClick={showAge}> show age</button>
+    <br></br>
+
     <input type="text" onChange={showText}></input>
+    <br></br>
+
+    <h2 id="name">{name}({age}) : {msg}</h2>
+    <UserName name = {name} />
+    <button onClick = {changeName}>Change</button>
+    <br></br>
+<hr></hr>
     </div>  
     );
 }
